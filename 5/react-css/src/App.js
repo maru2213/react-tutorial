0a04e8ae54591ae06943a8d319436f4e2a370714
maru2213 <25478531+maru2213@users.noreply.github.com>
2022-01-29 19:45:09 +0900

    Tailwind CSSの使用

--- 5/react-css/src/App.js
+++ 5/react-css/src/App.js
@@ -2,6 +2,8 @@ import { CssModules } from "./components/CssModules"
 import { Emotion } from "./components/Emotion";
 import { StyledComponents } from "./components/StyledComponents";
 import { StyledJsx } from "./components/StyledJsx";
+import { TailwindCss } from "./components/TailwindCss";
+import "./index.css";
 
 export const App = () => {
   return (
@@ -10,6 +12,7 @@ export const App = () => {
       <StyledJsx />
       <StyledComponents />
       <Emotion />
+      <TailwindCss />
     </>
   );
 }
