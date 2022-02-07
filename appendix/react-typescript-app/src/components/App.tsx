322220809b6f4a1269f3f5a606d1ab800dbaded3
maru2213 <25478531+maru2213@users.noreply.github.com>
2022-02-07 15:53:39 +0900

    メモアプリを雑に実装

--- appendix/react-typescript-app/src/components/App.tsx
+++ appendix/react-typescript-app/src/components/App.tsx
@@ -1,5 +1,39 @@
+import { ChangeEvent, useState } from "react"
+
 export const App = () => {
+  const [text, setText] = useState<string>("")
+  const [memos, setMemos] = useState<string[]>([])
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  const onClickAddButton = () => {
+    const newMemos = [...memos]
+    newMemos.push(text)
+    setMemos(newMemos)
+    setText("")
+  }
+
+  const onClickDeleteButton = (index: number) => {
+    const newMemos = [...memos]
+    newMemos.splice(index, 1)
+    setMemos(newMemos)
+  }
+
   return (
-    <h1>簡単メモアプリ</h1>
+    <>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <button onClick={onClickAddButton}>追加</button>
+      <ul>
+        {memos.map((memo, index) => (
+          <li>
+            <p>{memo}</p>
+            <button onClick={() => onClickDeleteButton(index)}>削除</button>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
