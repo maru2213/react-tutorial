fd08dc00ce0eefa4e3d98e338eead86f4585e0ed
maru2213 <25478531+maru2213@users.noreply.github.com>
2022-02-17 17:36:36 +0900

    カスタムフックの機能を実装

--- 基礎から学ぶReact/todo-app/src/hooks/useTodo.js
+++ 基礎から学ぶReact/todo-app/src/hooks/useTodo.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { ulid } from "ulid"
 import * as todoData from "../api/todos"
 
 export const useTodo = () => {
@@ -10,9 +11,31 @@ export const useTodo = () => {
     })
   }, [])
 
-  // const toggleTodoListItemStatus
-  // const addTodoListItem
-  // const deleteTodoListItem
+  const toggleTodoListItemStatus = (id) => {
+    const todo = todoList.find((item) => item.id === id)
+    const newTodo = {...todo, done: !todo.done}
+    todoData.updateTodoData(id, newTodo).then(() => {
+      const newList = todoList.map((item) => item.id === id ? newTodo : item)
+      setTodoList(newList)
+    })
+  }
 
-  return {todoList}
+  const addTodoListItem = (todoContent) => {
+    const todo = {
+      id: ulid(),
+      content: todoContent,
+      done: false
+    }
+    todoData.addTodoData(todo).then(() => {
+      setTodoList([todo, ...todoList])
+    })
+  }
+
+  const deleteTodoListItem = (id) => {
+    todoData.deleteTodoData(id).then(() => {
+      setTodoList(todoList.filter((item) => item.id !== id))
+    })
+  }
+
+  return {todoList, toggleTodoListItemStatus, addTodoListItem, deleteTodoListItem}
 }
